8bd8f8dd90626aca7ac87f0cb787e96c24bdd5de
Artur Gevorgyan <artur93gev@gmail.com>
2021-01-27 00:16:03 +0300

    feat(types): add typescript support
    Properly typing of "createVirtualModel" is not possible with typescript, input/output is typed
    correctly. Intermediate callback function has no types :(

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,5 +5,5 @@ export interface IModel<T> {
 }
 
 export function createModel<T>(value: T): IModel<T>;
-export function createVirtualModel<K, C, T extends IModel<C>>(...models: T[]): (...values: C[]) => IModel<K>;
+export function createVirtualModel<K, T extends IModel<K>[]>(...models: T): (callback: (...values: any) => any) => IModel<K>;
 export function publishModel<T>(model: IModel<T>, value: T | ((nextValue: T) => T)): void;
