2b0e334e7ef1b766137121dc40215280b31d153f
Artur Gevorgyan <artur93gev@gmail.com>
2021-01-31 22:42:50 +0300

    refactor(virtual model): change the creation phase of virtual model

--- src/api/configure.js
+++ src/api/configure.js
@@ -1,52 +1,58 @@
 import { isEqual, createDefault } from '../utils';
-import { register, subscribe, publish } from '../pubsub';
+import { registerEvent, updateEvent, subscribe, publish } from '../pubsub';
 
-import { generateId  } from './utils';
+import { generateId, collectState  } from './utils';
 
 const createModel = (defaultData, options) => {
   const event = generateId();
 
-  const model = register(event, defaultData, options);
+  const model = registerEvent(event, defaultData, options);
 
   return {
     event,
     getState: () => model.lastState,
+    setOptions: options => updateEvent(event, options),
     subscribe: (callback, needPrevious) => subscribe(event, callback, needPrevious),
   };
 }
 
-const createVirtualModel = (...models) => {
-  return (handler, options) => {
+const createVirtualModel = (handler, { models = [], ...options } = {}) => {
+  const virtualEvent = createDefault(handler(...collectState(models)), options);
 
-    const virtualEvent = createDefault(handler(...models.map(model => model.getState())), options);
+  const updateHandler = neededModels => {
+    const nextState = handler(...collectState(neededModels));
+    if (virtualEvent.options.fireDuplcates || !isEqual(nextState, virtualEvent.lastState)) {
+      virtualEvent.lastState = nextState;
+      virtualEvent.subscribers.forEach(subscriber => subscriber(virtualEvent.lastState));
+    }
+  }
 
-    models.map(model =>
-      model.subscribe(() => {
-        const nextState = handler(...models.map(model => model.getState()));
-        if (virtualEvent.options.fireDuplcates || !isEqual(nextState, virtualEvent.lastState)) {
-          virtualEvent.lastState = nextState;
-          virtualEvent.subscribers.forEach(subscriber => subscriber(virtualEvent.lastState));
-        }
-      }),
-    );
+  let subscriptions = models.map(model => model.subscribe(() => updateHandler(models)));
 
-    return ({
-      event: models.map(({ event }) => event),
-      getState: () => virtualEvent.lastState,
-      subscribe: (callback, needPrevious) => {
-        needPrevious && callback(virtualEvent.lastState);
-        virtualEvent.subscribers.push(callback);
-        return () => {
-          virtualEvent.subscribers = virtualEvent.subscribers(subscriber => subscriber !== callback);
-        }
-      },
-    });
-  };
+  return ({
+    event: models.map(({ event }) => event),
+    getState: () => virtualEvent.lastState,
+    subscribe: (callback, needPrevious) => {
+      needPrevious && callback(virtualEvent.lastState);
+      virtualEvent.subscribers.push(callback);
+      return () => {
+        virtualEvent.subscribers = virtualEvent.subscribers(subscriber => subscriber !== callback);
+      }
+    },
+    setOptions: ({
+      models: newModels = [],
+      ...newOptions
+    }) => {
+      virtualEvent.options = { ...virtualEvent.options, ...newOptions };
+      if (newModels.length) {
+        subscriptions.map(unsubscribe => unsubscribe());
+        subscriptions = newModels.map(model => model.subscribe(() => updateHandler(newModels)));
+      }
+    }
+  });
 }
 
-const publishModel = (model, data) => {
-  publish(model.event, data, { model });
-}
+const publishModel = (model, data) => publish(model.event, data, { model });
 
 export {
   createModel,
