6f66a5d7c6c6b5430b027d853d3755e3b458e94f
Artur Gevorgyan <artur93gev@gmail.com>
2021-08-19 21:15:43 +0200

    refactor(virtual model): changing default state
    the virtual state executor must not be called when initializing

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,12 @@
 // TODO: provide an option for changing comparison logic
 const isEqual = (next, prev) => Object.is(next, prev);
 
-const createDefault = (lastState, options = {}) => ({ lastState, options, subscribers: [] });
+const defaultState = {};
+
+const createDefault = ({ lastState = defaultState, options = {} }) => ({ lastState, options, subscribers: [] });
+
+const isDefault = state => state === defaultState;
 
 const noop = () => {};
 
-export { isEqual, createDefault, noop };
+export { isEqual, createDefault, noop, isDefault };
